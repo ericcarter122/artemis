b19383af00c6e4dd0edab99eef7ca9edfd2ae711
Tryptophan <jagreenway12@gmail.com>
2016-07-25 00:07:19 -0500

    added scripts for more components

---
+++ Assets/Scripts/Ship/HUD.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HUD : MonoBehaviour {
+
+    private Targeting targeting;
+    private Engines engines;
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        targeting = GetComponent<Targeting>();
+        engines = GetComponent<Engines>();
+        rb = GetComponent<Rigidbody>();   
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnGUI() {
+
+        GUILayout.Label("VEL: " + rb.velocity.z + " m/s");
+
+        // Move to navigation script
+        float heading = Vector3.Angle(transform.forward, engines.station.forward);
+        GUILayout.Label("HDG: " + heading + " deg");
+
+        GUILayout.Label("FLT ASST: " + engines.flightAssist);
+
+        GUILayout.Label("Target: " + targeting.target.name);
+
+        GameObject target = targeting.target;
+        GUILayout.Label("Distance: " + Vector3.Distance(transform.position, target.transform.position) + " m");
+    }
+}
---
+++ Assets/Scripts/Ship/Targeting.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Targeting : MonoBehaviour {
+
+    public List<GameObject> targets;
+    public GameObject target;
+
+    private int targetIndex;
+
+    private float nextPress = 0.0F;
+    private float pressRate = 0.1F;
+
+	// Use this for initialization
+	void Start () {
+        targets = new List<GameObject>(GameObject.FindGameObjectsWithTag("Ship"));
+        targetIndex = 0;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        bool nextTarget = Input.GetButton("Next Target");
+        List<GameObject> newTargets = new List<GameObject>(GameObject.FindGameObjectsWithTag("Ship"));
+
+        // Check if target pool changes
+        //if (!targets.Equals(newTargets)) {
+        //    targets = newTargets;
+
+        //    if (targets.Count > 0) {
+
+        //        GameObject newTarget = null;
+
+        //        for (int i = 0; i < targets.Count; i++) {
+        //            if (target.Equals(targets[i])) {
+        //                newTarget = targets[i];
+        //                targetIndex = i;
+        //                break;
+        //            }
+        //        }
+
+        //        target = newTarget;
+        //    }
+        //}
+
+        // Check if target is changed
+        if (nextTarget && targets.Count > 0 && Time.time > nextPress) {
+
+            nextPress = Time.time + pressRate;
+            int nextIndex = targetIndex++;
+
+            if (nextIndex == targets.Count) {
+                nextIndex = 0;
+                targetIndex = nextIndex;
+            }
+
+            target = targets[nextIndex];
+        }
+	}
+}
