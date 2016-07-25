e5df7cc8323548f04553b5c50f9c73d9bf3916e8
Jacob Greenway <jagreenway12@gmail.com>
2016-07-24 20:39:25 -0500

    init commit

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera : MonoBehaviour {
+
+	public float speed;
+
+	private float yaw = 0.0F;
+    private float pitch = 0.0F;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		yaw += speed * Input.GetAxis("Mouse X");
+		pitch -= speed * Input.GetAxis("Mouse Y");
+
+		transform.eulerAngles = new Vector3(pitch, yaw, 0.0F);
+	}
+}
