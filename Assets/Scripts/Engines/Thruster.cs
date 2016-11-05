5490aa1a90e9e155ba736b443c05c956546896af
Jacob Greenway <jagreenway12@gmail.com>
2016-11-05 15:45:46 -0500

    modified HUD test and added add force to thruster

--- Assets/Scripts/Engines/Thruster.cs
+++ Assets/Scripts/Engines/Thruster.cs
@@ -1,8 +1,20 @@
 using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
 public class Thruster : MonoBehaviour{
     public enum ThusterType { nitrogen, hydrogen };
 
     public float maxThrust;
+
+    Rigidbody rb;
+
+    public void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void AddForce(Vector3 force) {
+        rb.AddForceAtPosition(force, transform.position);
+    }
 
     // TODO: add animation/particle effects per thruster
 }
