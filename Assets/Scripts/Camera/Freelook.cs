09a09d93b6d62b48e9a10f72ed994c4f323201d0
Jacob Greenway <jagreenway12@gmail.com>
2017-03-11 13:58:36 -0600

    wip - add mfd models, revert back from using interactable cockpit

--- Assets/Scripts/Camera/Freelook.cs
+++ Assets/Scripts/Camera/Freelook.cs
@@ -59,4 +59,6 @@ public class Freelook : MonoBehaviour {
         }
         return Mathf.Clamp(angle, min, max);
     }
+
+    // todo: set the freelook to center with originalRotation
 }
--- Assets/Scripts/Camera/Zoom.cs
+++ Assets/Scripts/Camera/Zoom.cs
@@ -6,11 +6,12 @@ public class Zoom : MonoBehaviour {
 
 	public float minFov = 15F;
 	public float maxFov = 90F;
-	public float sensitivity = 10F; 
+	public float sensitivity = 10F;
 
 	void Update () {
 		float fov = Camera.main.fieldOfView;
 		fov += Input.GetAxis("Mouse ScrollWheel") * sensitivity;
+
 		fov = Mathf.Clamp(fov, minFov, maxFov);
 		Camera.main.fieldOfView = fov;
 	}
